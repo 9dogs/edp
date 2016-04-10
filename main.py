b174affff4b99b6cfeef7dfcbb71ed365178d548
Mikhail Knyazev <ninedogs@yandex.ru>
2016-04-10 19:03:18 +0300

    Добавлен фитинг

--- main.py
+++ main.py
@@ -9,9 +9,22 @@ import pandas as pd
 from pyqtgraph.Qt import QtGui, QtCore
 import pyqtgraph as pg
 
+from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt4agg import NavigationToolbar2QT as NavigationToolbar
+import matplotlib.pyplot as plt
+
+
+from lmfit import Model, Parameters
+import seaborn as sns
+sns.set(style="whitegrid")
+
 
 LASER_TITLE = 'Laser'
 DELAY_TITLE = 'Time'
+
+
+def echo_decay(x, y0, A, t2):
+    return y0 + A*np.exp(-4 * x / t2)
 
 
 class MainWindow(QtGui.QMainWindow):
@@ -29,7 +42,12 @@ class MainWindow(QtGui.QMainWindow):
         self.data = pd.DataFrame()
 
         self._central_widget = QtGui.QWidget()
-        self._main_layout = QtGui.QVBoxLayout()
+
+        self._graph_tabs_widget = QtGui.QTabWidget()
+        self._graph_tabs_widget.currentChanged.connect(self._on_tab_changed)
+
+        self._main_layout = QtGui.QHBoxLayout()
+        self._pyqtgraph_layout = QtGui.QVBoxLayout()
 
         # GraphLayoutWidget с двумя графиками: гистограмма лазера и эхо
         self._plot_area = pg.GraphicsLayoutWidget()
@@ -46,8 +64,98 @@ class MainWindow(QtGui.QMainWindow):
         self._select_region.setZValue(-10)
         self._select_region.sigRegionChanged.connect(self._calc_curve_data)
 
+        # Посчитана ли статистика фита
+        self._fit_stats = False
+
+        # Результирующие графики
+
+        # График фита
+        fitting_tab = QtGui.QWidget()
+        # a figure instance to plot on
+        self._fitting_figure = plt.figure()
+        self._fitting_canvas = FigureCanvas(self._fitting_figure)
+        # toolbar
+        fitting_toolbar = NavigationToolbar(self._fitting_canvas, self)
+        # fit parameters
+        params_layout = QtGui.QHBoxLayout()
+        # y0
+        y0_form = QtGui.QFormLayout()
+        self.y0_min, self.y0_max, self.y0_value, self.y0_fixed = QtGui.QDoubleSpinBox(), QtGui.QDoubleSpinBox(), QtGui.QDoubleSpinBox(), QtGui.QCheckBox()
+        for spin_box in [self.y0_min, self.y0_max, self.y0_value]:
+            spin_box.setMaximum(99999)
+            spin_box.setMinimum(-99999)
+            spin_box.setSingleStep(0.001)
+        y0_form.addRow(QtGui.QLabel("y0 (~5000)"))
+        y0_form.addRow("Min", self.y0_min)
+        y0_form.addRow("Max", self.y0_max)
+        y0_form.addRow("Value", self.y0_value)
+        y0_form.addRow("Fixed?", self.y0_fixed)
+
+        params_layout.addLayout(y0_form)
+
+        # A
+        A_form = QtGui.QFormLayout()
+        self.A_min, self.A_max, self.A_value, self.A_fixed = QtGui.QDoubleSpinBox(), QtGui.QDoubleSpinBox(), QtGui.QDoubleSpinBox(), QtGui.QCheckBox()
+        for spin_box in [self.A_min, self.A_max, self.A_value]:
+            spin_box.setMaximum(99999)
+            spin_box.setMinimum(-99999)
+            spin_box.setSingleStep(0.001)
+        A_form.addRow(QtGui.QLabel("A (~500)"))
+        A_form.addRow("Min", self.A_min)
+        A_form.addRow("Max", self.A_max)
+        A_form.addRow("Value", self.A_value)
+        A_form.addRow("Fixed?", self.A_fixed)
+
+        params_layout.addLayout(A_form)
+
+        # t2
+        t2_form = QtGui.QFormLayout()
+        self.t2_min, self.t2_max, self.t2_value, self.t2_fixed = QtGui.QDoubleSpinBox(), QtGui.QDoubleSpinBox(), QtGui.QDoubleSpinBox(), QtGui.QCheckBox()
+        for spin_box in [self.t2_min, self.t2_max, self.t2_value]:
+            spin_box.setMaximum(99999)
+            spin_box.setMinimum(-99999)
+            spin_box.setSingleStep(0.001)
+        t2_form.addRow(QtGui.QLabel("t2 (~500)"))
+        t2_form.addRow("Min", self.t2_min)
+        t2_form.addRow("Max", self.t2_max)
+        t2_form.addRow("Value", self.t2_value)
+        t2_form.addRow("Fixed?", self.t2_fixed)
+
+        params_layout.addLayout(t2_form)
+
+        # Initial guesses
+        self.y0_value.setValue(5000)
+        self.A_value.setValue(500)
+        self.t2_value.setValue(500)
+
+        fit_layout = QtGui.QVBoxLayout()
+        fit_layout.addWidget(fitting_toolbar)
+        fit_layout.addWidget(self._fitting_canvas)
+        fit_layout.addLayout(params_layout)
+        fitting_tab.setLayout(fit_layout)
+
+        self._graph_tabs_widget.addTab(fitting_tab, 'Фит')
+
+        # График зависимости t2 от точки старта фита
+        fitting_stats_tab = QtGui.QWidget()
+        # a figure instance to plot on
+        self._fitting_stats_figure = plt.figure()
+        self._fitting_stats_canvas = FigureCanvas(self._fitting_stats_figure)
+        fitting_stats_toolbar = NavigationToolbar(self._fitting_stats_canvas, self)
+
+        fit_stats_layout = QtGui.QVBoxLayout()
+        fit_stats_layout.addWidget(fitting_stats_toolbar)
+        fit_stats_layout.addWidget(self._fitting_stats_canvas)
+        fitting_stats_tab.setLayout(fit_stats_layout)
+
+        self._graph_tabs_widget.addTab(fitting_stats_tab, 'Зависимость фита')
+
+        # Фит репорт
+        self.fit_report_tab = QtGui.QTextEdit()
+        self._graph_tabs_widget.addTab(self.fit_report_tab, 'Отчет')
+
         # Бины для гистограммы лазера
-        self._bins_widget = QtGui.QSpinBox()
+        self._bins_widget = QtGui.QDoubleSpinBox()
         self._bins_widget.setMinimum(1)
         self._bins_widget.setMaximum(1000)
         self._bins_widget.setValue(200)
@@ -61,16 +169,29 @@ class MainWindow(QtGui.QMainWindow):
         self._copy_data_btn = QtGui.QPushButton("Скопировать в буфер")
         self._copy_data_btn.clicked.connect(self._on_copy_data_clicked)
 
+        # Кнопка фита
+        self._fit_btn = QtGui.QPushButton("Фитировать")
+        self._fit_btn.clicked.connect(self._on_fit_btn_clicked)
+
+        # Галочка логарифмического масштаба
+        self._log_checkbox = QtGui.QCheckBox("Log scale")
+        self._log_checkbox.stateChanged.connect(self._log_scale_changed)
+
         # Layout для выбора графиков и бинов
         self._aux_layout = QtGui.QHBoxLayout()
         self._aux_layout.addWidget(self._bins_widget)
         self._aux_layout.addWidget(self._graph_select)
+        self._aux_layout.addWidget(self._log_checkbox)
         self._aux_layout.addStretch(5)
+        self._aux_layout.addWidget(self._fit_btn)
         self._aux_layout.addWidget(self._copy_data_btn)
 
-        self._main_layout.addWidget(self._plot_area)
-        self._main_layout.addLayout(self._aux_layout)
+        self._pyqtgraph_layout.addWidget(self._plot_area)
+        self._pyqtgraph_layout.addLayout(self._aux_layout)
 
+        # Установка лэйаутов
+        self._main_layout.addLayout(self._pyqtgraph_layout)
+        self._main_layout.addWidget(self._graph_tabs_widget)
         self._central_widget.setLayout(self._main_layout)
         self.setCentralWidget(self._central_widget)
 
@@ -78,16 +199,132 @@ class MainWindow(QtGui.QMainWindow):
         self._create_menu()
         self._create_status_bar()
 
+    def _on_tab_changed(self, tab):
+        pass
+
+    def _log_scale_changed(self, state):
+        if state == 0:
+            # Log scale unchecked
+            self.echo_plot.setLogMode(x=False)
+            self._v_line.setValue(10**self._v_line.value())
+        elif state == 2:
+            # Log scale checked
+            self.echo_plot.setLogMode(x=True)
+            if self._v_line.value() <= 0:
+                new_line_pos = 1
+            else:
+                new_line_pos = np.log10(self._v_line.value())
+            self._v_line.setValue(new_line_pos)
+            self.echo_plot.setXRange(0, 3)
+
+    def _calc_fit_start_dep_curve(self):
+        t2_array = []
+        x_array = []
+        # Fit value
+        fit_variable_name = self._graph_select.currentText()
+        for start_fit_from in self.means[DELAY_TITLE]:
+            start_fit_from = float(start_fit_from)
+            fit_means = self.means[start_fit_from < self.means[DELAY_TITLE]]
+            fit_std = self.std[start_fit_from < self.std[DELAY_TITLE]]
+
+            params = self._init_params()
+
+            model = Model(echo_decay, independent_vars=['x'])
+            try:
+                result = model.fit(fit_means[fit_variable_name], x=fit_means[DELAY_TITLE],
+                                   params=params, weights=1 / fit_std[LASER_TITLE] ** 2)
+                t2_array.append(result.params.get('t2').value)
+                x_array.append(start_fit_from)
+            except TypeError:
+                break
+
+        ax = self._fitting_stats_figure.add_subplot(111)
+        ax.plot(x_array, t2_array, '--bo', alpha=0.9)
+        ax.set_title("Зависимость $T_2$ от точки старта фитирования")
+        # refresh canvas and tight layout
+        self._fitting_stats_canvas.draw()
+        self._fitting_stats_figure.tight_layout()
+        self._fit_stats = True
+
     def _on_v_line_pos_changed(self):
         self._calc_curve_data()
+        # self.statusBar().showMessage("Мин. лазер: {:.2f}\tМакс. лазер: {:.2f}\tСредний: {:.2f}\tОтсеяно: {:.2f}%\tСигма: {:.2f}\tСтарт: {:.5f}".format(laser_min, laser_max, laser_mean, del_rows_count, laser_std, self._v_line.value()))
 
     def _on_copy_data_clicked(self):
-        if not self.averaged_data.empty:
-            self.averaged_data.to_clipboard()
+        if not self.means.empty:
+            self.means.to_clipboard()
+
+    def _init_params(self):
+        # Params
+        params = Parameters()
+        y0_min = self.y0_min.value() if self.y0_min.value() > 0 else None
+        y0_max = self.y0_max.value() if self.y0_max.value() > 0 else None
+        y0_value = self.y0_value.value()
+        y0_vary = False if self.y0_fixed.isChecked() else True
+        params.add('y0', min=y0_min, max=y0_max, value=y0_value, vary=y0_vary)
+
+        A_min = self.A_min.value() if self.A_min.value() > 0 else None
+        A_max = self.A_max.value() if self.A_max.value() > 0 else None
+        A_value = self.A_value.value()
+        A_vary = False if self.A_fixed.isChecked() else True
+        params.add('A', min=A_min, max=A_max, value=A_value, vary=A_vary)
+
+        t2_min = self.t2_min.value() if self.t2_min.value() > 0 else None
+        t2_max = self.t2_max.value() if self.t2_max.value() > 0 else None
+        t2_value = self.t2_value.value()
+        t2_vary = False if self.t2_fixed.isChecked() else True
+        params.add('t2', min=t2_min, max=t2_max, value=t2_value, vary=t2_vary)
+
+        return params
+
+    def _fit_data(self):
+        # clean plots
+        self._fitting_figure.clear()
+        self._fitting_stats_figure.clear()
+
+        # Check if x scale is log
+        if self._log_checkbox.isChecked():
+            start_fit_from = 10**float(self._v_line.value())
+        else:
+            start_fit_from = float(self._v_line.value())
+        fit_means = self.means[start_fit_from < self.means[DELAY_TITLE]]
+        fit_std = self.std[start_fit_from < self.std[DELAY_TITLE]]
+
+        # Fit value
+        fit_variable_name = self._graph_select.currentText()
+
+        params = self._init_params()
+
+        model = Model(echo_decay, independent_vars=['x'])
+        result = model.fit(fit_means[fit_variable_name], x=fit_means[DELAY_TITLE], params=params,
+                           weights=1 / fit_std[LASER_TITLE] ** 2)
+
+        time = np.linspace(start_fit_from, self.means[DELAY_TITLE].max(), 1000)
+
+        ax = self._fitting_figure.add_subplot(111)
+        # plot data
+        ax.errorbar(self.means[DELAY_TITLE], self.means[fit_variable_name], yerr=self.std[fit_variable_name],
+                    fmt='o', capthick=2, ecolor='b', alpha=0.5)
+        ax.plot(time, echo_decay(x=time, **result.values), '--r', alpha=0.9)
+        ax.set_title("$T_2$: {:0.2f}".format(result.values['t2']))
+        # refresh canvas and tight layout
+        self._fitting_canvas.draw()
+        self._fitting_figure.tight_layout()
+        # Populating report
+        self.fit_report_tab.setText(result.fit_report())
+        # Populating spinboxes
+        self.y0_value.setValue(result.values['y0'])
+        self.A_value.setValue(result.values['A'])
+        self.t2_value.setValue(result.values['t2'])
+
+    def _on_fit_btn_clicked(self):
+        self._fit_data()
+        if not self._fit_stats:
+            self._calc_fit_start_dep_curve()
 
     def _update_line_pos(self):
         try:
-            self._v_line.setPos(self.averaged_data.mean())
+            self._v_line.setPos(self.means.mean())
         except ValueError:
             pass
 
@@ -111,7 +348,8 @@ class MainWindow(QtGui.QMainWindow):
             curve = pg.PlotCurveItem(x, y, stepMode=True, fillLevel=0, brush=(255, 255, 255, 40))
             self.laser_plot.clear()
             self.laser_plot.addItem(curve)
-            self._select_region.setRegion([self.data[LASER_TITLE].min(), self.data[LASER_TITLE].max()])
+            std = self.data[LASER_TITLE].std()
+            self._select_region.setRegion([self.data[LASER_TITLE].mean() - std, self.data[LASER_TITLE].mean() + std])
             self.laser_plot.addItem(self._select_region)
 
     def _create_menu(self):
@@ -131,21 +369,30 @@ class MainWindow(QtGui.QMainWindow):
         Calculates data for echo plot
         """
         laser_min, laser_max = self._select_region.getRegion()
-        # good data indices
-        good_data = self.data[(laser_min < self.data[LASER_TITLE]) & (self.data[LASER_TITLE] < laser_max)]
-        mean = good_data[LASER_TITLE].mean()
-        std = good_data[LASER_TITLE].std()
+        # Getting data between laser_min and laser_max
+        good_data = self.data[(float(laser_min) < self.data[LASER_TITLE]) & (self.data[LASER_TITLE] < float(laser_max))]
+        del_rows_count = (self.data.shape[0] - good_data.shape[0])/self.data.shape[0] * 100
+        laser_mean = good_data[LASER_TITLE].mean()
+        laser_std = good_data[LASER_TITLE].std()
+
+        if self._log_checkbox.isChecked():
+            start_fit_from = 10 ** float(self._v_line.value())
+        else:
+            start_fit_from = float(self._v_line.value())
 
         self.statusBar().showMessage(
-            "Мин. лазер: {:.2f}\tМакс. лазер: {:.2f}\tСредний: {:.2f}\tСигма: {:.2f}\tСтарт: {:.5f}".format(laser_min,
-                                                                                                            laser_max,
-                                                                                                            mean,
-                                                                                                            std,
-                                                                                                            self._v_line.value()))
+            "Мин. лазер: {:.2f}\tМакс. лазер: {:.2f}\tСредний: {:.2f}\tОтсеяно: {:.2f}%\tСигма: {:.2f}\tСтарт: {:.5f}".format(laser_min, laser_max, laser_mean, del_rows_count, laser_std, start_fit_from))
 
-        self.averaged_data = good_data.groupby(DELAY_TITLE).mean().reset_index()
+        averaged_data = good_data.groupby(DELAY_TITLE)
+        self.means = averaged_data.mean().reset_index()
+        self.std = averaged_data.std().reset_index()
+        self.count = averaged_data.count().reset_index()
+
         self.echo_plot.clear()
-        self.echo_plot.plot(self.averaged_data[DELAY_TITLE], self.averaged_data[self._graph_select.currentText()])
+        self.echo_plot.plot(self.means[DELAY_TITLE], self.means[self._graph_select.currentText()], pen=(200, 200, 200), symbolBrush=(230, 0, 0, 0.8*255), symbolPen='w')
+        err = pg.ErrorBarItem(x=self.means[DELAY_TITLE], y=self.means[self._graph_select.currentText()],
+                              height=2*self.std[self._graph_select.currentText()], beam=0.5)
+        #self.echo_plot.addItem(err)
         self.echo_plot.addItem(self._v_line)
         self._update_line_pos()
 
@@ -153,9 +400,12 @@ class MainWindow(QtGui.QMainWindow):
         QtGui.QMessageBox.about(self, "About EDP", "fgsfds")
 
     def _clear_all(self):
-        '''Clear all plots and selects'''
+        """Clear all plots and selects"""
         self.laser_plot.clear()
         self._graph_select.clear()
+        self._fit_stats = False
+        self._fitting_figure.clear()
+        self._fitting_stats_figure.clear()
 
     def _open(self):
         from os.path import basename
@@ -167,6 +417,7 @@ class MainWindow(QtGui.QMainWindow):
             self.setWindowTitle("EDP - " + basename(filename))
             self._fill_graph_select()
             self._plot_hist()
+            self._calc_curve_data()
             # except Exception as e:
             # self.statusBar().showMessage("Loading failed: {}".format(e))
 
@@ -176,7 +427,8 @@ class MainWindow(QtGui.QMainWindow):
                 y, x = np.histogram(self.data[LASER_TITLE], bins=self._bins_widget.value())
                 curve = pg.PlotCurveItem(x, y, stepMode=True, fillLevel=0, brush=(255, 255, 255, 40))
                 self.laser_plot.addItem(curve)
-                self._select_region.setRegion([self.data[LASER_TITLE].min(), self.data[LASER_TITLE].max()])
+                std = self.data[LASER_TITLE].std()
+                self._select_region.setRegion([self.data[LASER_TITLE].mean()-std, self.data[LASER_TITLE].mean()+std])
                 self.laser_plot.addItem(self._select_region)
             except KeyError:
                 self.statusBar().showMessage("Laser field not found in data provided. Nothing to plot.")
